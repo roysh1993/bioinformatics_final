33bb8ff9c7c6cfec0a61981ab945c1af1e0510d4
royshafir <royshafir8@gmail.com>
2020-03-11 19:03:02 +0200

    minor change

--- generate_coverage.py
+++ generate_coverage.py
@@ -93,7 +93,9 @@ def get_fragments_by_index(fastq_file,frag_indexes, output_filename= OUTPUT_FRAG
 									# print(frag_indexes.pop(0))
 									out.write(frag_data)
 									out.write("\n")
+									frag_indexes.pop(0)
 									frag_identifiers.append(frag_identifier[:-1])
+
 
 						else:
 							i = 0
@@ -165,22 +167,28 @@ def single_ineration_per_corr(fastq_files, coverage_ratio):
 
 
 if __name__ == '__main__':
-	import argparse
+	# import argparse
+	#
+	# parser = argparse.ArgumentParser(description='Generate new converage dataset',
+	# 								 formatter_class=argparse.RawTextHelpFormatter)
+	#
+	# parser.add_argument("-1",'--file1', type=str, metavar='<fastq1>',
+	# 					help='Specify fastq files for recalculation, comma seperated.',
+	# 					required=True)
+	# parser.add_argument("-2",'--file2', type=str, metavar='<fastq2>',
+	# 					help='Specify fastq files for recalculation, comma seperated.',
+	# 					required=True)
+	#
+	# parser.add_argument('--coverage', type=float, metavar='<coverage>',
+	# 					help='Specify new coverage.',
+	# 					required=True)
+	#
+	# args = parser.parse_args()
+	# fastq_files = [args.file1, args.file2]
+	# single_ineration_per_corr(fastq_files,args.coverage)
 
-	parser = argparse.ArgumentParser(description='Generate new converage dataset',
-									 formatter_class=argparse.RawTextHelpFormatter)
 
-	parser.add_argument("-1",'--file1', type=str, metavar='<fastq1>',
-						help='Specify fastq files for recalculation, comma seperated.',
-						required=True)
-	parser.add_argument("-2",'--file2', type=str, metavar='<fastq2>',
-						help='Specify fastq files for recalculation, comma seperated.',
-						required=True)
 
-	parser.add_argument('--coverage', type=float, metavar='<coverage>',
-						help='Specify new coverage.',
-						required=True)
-
-	args = parser.parse_args()
-	fastq_files = [args.file1, args.file2]
-	single_ineration_per_corr(fastq_files,args.coverage)
+	fastq_files =["data/tiny_frag_1.fastq", "data/tiny_frag_2.fastq"]
+	coverage = 0.9
+	single_ineration_per_corr(fastq_files, coverage)
