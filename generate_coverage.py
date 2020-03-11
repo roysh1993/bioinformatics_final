1d7cb01ac2c87d00a1d8e39d2a9eaf819d7fc518
royshafir <royshafir8@gmail.com>
2020-03-11 19:41:19 +0200

    minor change

--- generate_coverage.py
+++ generate_coverage.py
@@ -4,10 +4,10 @@ import random
 import subprocess
 import subprocess
 import assembly_stats
-OUTPUT_FRAG_1_FILE =  "/data/roy/bio_informatics/Staphylococcus_aureus/frag_1.fastq"
-OUTPUT_FRAG_2_FILE =  "/data/roy/bio_informatics/Staphylococcus_aureus/frag_2.fastq"
+OUTPUT_FRAG_1_FILE =  "data/output_frag_1.fastq"
+OUTPUT_FRAG_2_FILE =  "data/output_frag_2.fastq"
 SPADES_EXE_LOCATION = "/data/roy/bio_informatics/SPAdes-3.12.0-Linux/bin/spades.py"
-OUTPUT_DIR = "./output_dir"
+OUTPUT_DIR = "output_dir"
 
 
 
