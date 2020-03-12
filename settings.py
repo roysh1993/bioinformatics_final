6e022ef4876fd670d889c85e238838ebf8fddb89
royshafir <royshafir8@gmail.com>
2020-03-12 20:19:06 +0200

    minor change (debugging)

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@ SPADES_EXE_LOCATION = "/data/roy/bio_informatics/SPAdes-3.12.0-Linux/bin/spades.
 OUTPUT_DIR = "output_dir"
 #constants
 START_COV = 0.1
-END_COV = 0.4
+END_COV = 0.23
 COV_INTERVAL = 0.1
-NUM_OF_EPOCHS = 3
+NUM_OF_EPOCHS = 2
 
